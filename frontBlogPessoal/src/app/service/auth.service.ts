768a250ddd9e0b08e0314838ee9325a50790d0fd
Ana Patricia da Silva <you@example.com>
2021-09-04 13:28:32 -0300

    Componente inicio sem consumo

--- frontBlogPessoal/src/app/service/auth.service.ts
+++ frontBlogPessoal/src/app/service/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
 import { User } from '../model/User';
 import { UserLogin } from '../model/UserLogin';
 
@@ -20,7 +21,16 @@ export class AuthService {
   }
 
   cadastrar(user: User): Observable<User> {
-    return this.http.post<User>('https://bloggenturma28.herokuapp.com/usuarios/cadastrar', user)
+    return this.http.post<User>('https://bloggenturma28.herokuapp.com/usuarios/cadastrar', user)    
+  }
 
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
